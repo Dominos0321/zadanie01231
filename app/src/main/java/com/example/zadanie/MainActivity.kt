9744bb696fa40fe886b3cbe7d58f962e2dd8249a
Dominos0321 <dsch792@gmail.com>
2022-05-18 20:44:26 +0200

    Koniec

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -15,52 +15,65 @@ class MainActivity : AppCompatActivity() {
         val tekst = findViewById<TextView>(R.id.text1);
         val widok = findViewById<CheckBox>(R.id.checkBox);
         val ile = findViewById<EditText>(R.id.editTextTextPersonName);
+        var x=0
 
-        var x = 0;
+
         findViewById<Button>(R.id.button2).setOnClickListener {
-            if (x == 0) {
-                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.bongocat);
-                x++;
-                findViewById<TextView>(R.id.text1).text = "Bongo Cat";
-            } else if (x == 1) {
+
+
+           if (x == 0) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.cheems);
                 x++;
                 findViewById<TextView>(R.id.text1).text = "Cheems";
-            } else if (x == 2) {
+            } else if (x == 1) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.doge);
                 x++;
                 findViewById<TextView>(R.id.text1).text = "Doge";
-            } else if (x == 3) {
+            } else if (x == 2) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.swole_doge);
                 x++;
                 findViewById<TextView>(R.id.text1).text = "Swole Doge";
-            } else if (x == 4) {
+            } else if (x == 3) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.popcat);
-                findViewById<TextView>(R.id.text1).text = "pop cat";
+                findViewById<TextView>(R.id.text1).text = "Pop Cat";
             }
         }
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            if (x == 1) {
-                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.bongocat);
-                x--;
-                findViewById<TextView>(R.id.text1).text = "Bongo Cat";
-            } else if (x == 2) {
+
+            if (x == 0) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.bongocat)
+                findViewById<TextView>(R.id.text1).text ="Bongo Cat";
+
+            } else if (x == 1) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.cheems);
                 x--;
                 findViewById<TextView>(R.id.text1).text = "Cheems";
-            } else if (x == 3) {
+            } else if (x == 2) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.doge);
                 x--;
                 findViewById<TextView>(R.id.text1).text = "Doge";
-            } else if (x == 4) {
+            } else if (x == 3) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.swole_doge);
                 x--;
                 findViewById<TextView>(R.id.text1).text = "Swole Doge";
             }
+        }
+
+        widok.setOnCheckedChangeListener { x, isChecked ->
+
+            if (isChecked) {
+                var wis = ile.text.toString().toFloat();
+                obraz.alpha = wis
+            } else {
+                obraz.alpha = 1f;
+            }
+        }
 
 
-            widok1.setOnCheckedChangeListener {_, isChecked ->
+
+
+            widok1.setOnCheckedChangeListener {x, isChecked ->
                 if (isChecked) {
                     obraz.visibility = View.VISIBLE
                     tekst.visibility = View.VISIBLE
@@ -71,19 +84,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
 
-                widok.setOnCheckedChangeListener { compoundButton, isChecked ->
-
-                    if (isChecked) {
-                        var wis = ile.text.toString().toFloat();
-                        obraz.alpha = wis
-                    } else {
-                        obraz.alpha = 1f;
-                    }
-                }
 
 
             }
 
         }
     }
-}
